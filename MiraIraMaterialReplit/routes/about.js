058d3e81679b0830fc9298a6d082f090ee7ed903
Sure Rocha <126790749+surerocha@users.noreply.github.com>
2024-04-20 20:51:14 -0300

    Update about.js

--- MiraIraMaterialReplit/routes/about.js
+++ MiraIraMaterialReplit/routes/about.js
@@ -3,29 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next){
-  var characters =[
-               {
-                 name: 'Sure',
-                 role: 'Student'
-               },
-               {
-                 name: 'Zé Roberto',
-                 role: 'Headmaster'
-               },
-               {
-                 name: 'Caveirinha',
-                 role: 'Professor'
-               },
-               {
-                 name: 'Wladison',
-                 role: 'Student'
-               }
-             ];
-             var subheading = "I teste a empresa Replit";
-
-             res.render('about', {characters: characters, subheading: subheading});
-           });
-
-
+  res.render('about');
+  });
 
 module.exports = router;
