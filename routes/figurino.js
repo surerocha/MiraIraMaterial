23a6267706bef74381cca77d210c7216b652b41d
Sure Rocha <126790749+surerocha@users.noreply.github.com>
2024-04-20 20:40:36 -0300

    Delete routes directory

--- routes/figurino.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET Figurinos page. */
-router.get('/', function(req, res, next) {
-  res.render('figurino', {title: "Figurinos" });
-});
-
-module.exports = router;
